69cf7be51032a5e09f79d9707673d60952d9ced6
ikiqqq <riskidarliana76@gmail.com>
2021-08-19 12:37:43 +0700

    + memanggil function di JavaScript

--- Month 1/Week 1/Day 3/index.js
+++ Month 1/Week 1/Day 3/index.js
@@ -1,1 +1,22 @@
-console.log("Hallo world")
+const mahasiswa = {
+    namaDepan: "Riski",
+    namaBelakang: "Darliana",
+    nilaiMatematika: 80,
+    nilaiProgramming: 80,
+    nilaiBahasaInggris: 80,
+    totalNilai: function () {
+        return this.nilaiMatematika+this.nilaiProgramming+this.nilaiBahasaInggris
+    },
+
+    nilaiRataRata: function () {
+        return (this.nilaiMatematika+this.nilaiProgramming+this.nilaiBahasaInggris) / 3
+    },
+
+    namaLengkap: function () {
+        return this.namaDepan + " " + this.namaBelakang
+    },
+};
+
+console.log("Total Nilai:", mahasiswa.totalNilai());
+console.log("Total Nilai:", mahasiswa.nilaiRataRata());
+console.log("Nama Lengkap:", mahasiswa.namaLengkap());
