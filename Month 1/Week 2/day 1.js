4e735bab209808a168563eb1488b40f4518fb32f
ikiqqq <riskidarliana76@gmail.com>
2021-08-23 12:09:31 +0700

    materi minggu kedua hari pertama

---
+++ Month 1/Week 2/day 1.js
@@ -0,0 +1,42 @@
+// Pseudoceode 
+
+ 
+
+// Judul 
+
+// Program Menghitung_kecepatan_mobil 
+
+ 
+
+// Deklarasi 
+
+// Var jarak, waktu, kecepatan:integer; 
+
+ 
+
+// Implementasi 
+
+// Read (jarak) 
+
+// Read(waktu) 
+
+// IF waktu = 0 maka kecepatan sama dengan 0 
+
+// Kecepatan <- jarak/waktu; 
+
+// Write(kecepatan) 
+
+
+// implementasi pseuceode ke coding
+
+let kecepatan = 0;
+let jarak = 75;
+let waktu = 3;
+
+if(waktu == 0){
+    kecepatan = 0
+} else {
+    kecepatan = jarak / waktu
+}
+
+console.log(kecepatan)
